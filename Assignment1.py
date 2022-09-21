2dc7aefc00bd5860b9e0d7986a91343275292c2f
jignesh2413 <113872147+jignesh2413@users.noreply.github.com>
2022-09-21 23:32:17 +0530

    Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+"""
+Assignment- module-1 (SDLC)
+
+   1. What is software?
+      solutions of problems.
+      Collectios of Programms- we can operate it.
+
+   2. What are the types of applications? 
+      There are 3 types of Applications
+      Desktop
+      Web
+      Mobile Applications.
+
+   3. What is Programming?
+      Programming is a way to instruct the computer to perform various tasks.
+
+   4. What is python?
+      pyhton is a widely used general purpose, high level programming language.
+      python is a programming language that lets you work quickly and integrate systems more efficiently.""" 
